f34b2756ade79cee958ef77e516bd9a013724802
BardhKorca <bardh.korqa@gmail.com>
2023-03-14 17:28:38 +0100

    backend blog

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
 
-  http_basic_authenticate_with name: "Test", password: "Test123", only: :destroy
+  http_basic_authenticate_with name: "test", password: "test", only: :destroy
 
   def create
     @article = Article.find(params[:article_id])
